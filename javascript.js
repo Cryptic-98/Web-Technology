f961f8aea109acf7ed89b0c1f00994198a8b26e5
Goabaone <legendaryfan07@gmail.com>
2024-08-01 11:36:12 +0200

    Adding 2 numbers in javascript and looping

--- javascript.js
+++ javascript.js
@@ -3,4 +3,10 @@ alert('Go Home!')
 var number1 = 10
 var number2 = 10
 var total = number1 + number2
-document.write(number1 + " + " + number2 + " = " + total)
+document.write(number1 + " + " + number2 + " = " + total + "<br><br>")
+for(i = 0; i < 10; i++) {
+    document.write("I Love Web Technology 512" + "<br>")
+}
+for(i = 0; i < 10; i++) {
+    document.write("I Hate Web Technology 512" + "<br>")
+}
