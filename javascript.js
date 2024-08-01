93666d417eac5c873b0842f733e131b1a4f8b0ef
Goabaone <legendaryfan07@gmail.com>
2024-08-01 11:17:34 +0200

    Adding 2 numbers in javascript

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
-document.write("Hello World!")
+document.write("Hello World!" + '<br><br>')
+alert('Go Home!')
+var number1 = 10
+var number2 = 10
+var total = number1 + number2
+document.write(number1 + " + " + number2 + " = " + total)
