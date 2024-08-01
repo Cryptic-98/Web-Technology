d2eea4b4d24adb65655313851ec2dcdead12d57a
Goabaone <legendaryfan07@gmail.com>
2024-08-01 12:06:55 +0200

    Adding 2 numbers in javascript and looping

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
 document.write("Hello World!" + '<br><br>')
 alert('Go Home!')
+confirm('Would you like to continue?')
+var name = prompt('What is your name?')
+prompt('How old are you ' + name + '?')
 var number1 = 10
 var number2 = 10
 var total = number1 + number2
 document.write(number1 + " + " + number2 + " = " + total + "<br><br>")
-for(i = 0; i < 10; i++) {
+for(i = 0; i <= 10; i++) {
     document.write("I Love Web Technology 512" + "<br>")
 }
 for(i = 0; i < 10; i++) {
